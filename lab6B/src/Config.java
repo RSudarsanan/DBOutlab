dacbd354c6909dd5f79b583145422fde518e9ecc
RSudarsanan <rsudarsanan210725@gmail.com>
2018-08-28 18:42:32 +0530

    Basic code

---
+++ lab6B/src/Config.java
@@ -0,0 +1,9 @@
+
+/**
+ * Edit to provide your database credentials
+ */
+public class Config {
+	public static final String url = "jdbc:postgresql://localhost:7777/postgres";
+	public static final String user = "rsudarsanan";
+	public static final String password = "";
+}
